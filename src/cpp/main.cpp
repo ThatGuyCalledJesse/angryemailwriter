1837acfed68e2ffec579cca90f9a805627ab981b
ThatGuyCalledJesse <jesseswart08@gmail.com>
2022-10-15 18:17:04 +0200

    Timing
    Corrected the timing

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-double time_list[5] = {0.1, 0.2, 0.3, 0.4, 0.5};
+double time_list[5] = {100, 200, 300, 400, 500};
 bool isEnabled = false;
 
 // keybd_event(0x14, 0, KEYEVENTF_EXTENDEDKEY, 0);
