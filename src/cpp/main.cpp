b8f4d61751ce90bad5806d96319a675a523d8efb
ThatGuyCalledJesse <jesseswart08@gmail.com>
2022-10-15 18:05:53 +0200

    Test

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,9 +1,29 @@
-#include <WinUser.h>
+#include <windows.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+double time_list[5] = {0.1, 0.2, 0.3, 0.4, 0.5};
+bool isEnabled = false;
+
+// keybd_event(0x14, 0, KEYEVENTF_EXTENDEDKEY, 0);
+// keybd_event(0x14, 0, KEYEVENTF_KEYUP, 0);
 
 int main() {
-    while(true) {
-        
-    }
     
+    while(true) {
+        int RandIndex = rand() % 4; //generates a random number between 0 and 3
+        if (isEnabled == false) {
+            keybd_event(0x14, 0, KEYEVENTF_EXTENDEDKEY, 0);
+            isEnabled = true;
+            Sleep(time_list[RandIndex]);
+        }
+        else {
+            keybd_event(0x14, 0, KEYEVENTF_KEYUP, 0);
+            isEnabled = false;
+            Sleep(time_list[RandIndex]);
+        }
+    }
+
     return 0;
 }
